fa3cc53827bedfe7269e1940b9afb81c62eaf351
jvincec <jvincec@simple.biz>
2026-05-02 04:00:05 +0800

    chore: add dbmate migration baseline

---
+++ src/dbmate-migrations/20260502000100_baseline_existing_schema.sql
@@ -0,0 +1,9 @@
+-- migrate:up
+
+-- Baseline marker for automated migrations.
+-- Existing production/develop schema changes before this point were applied manually.
+-- New automated schema migrations should be added after this file.
+
+-- migrate:down
+
+-- No rollback for baseline marker.
